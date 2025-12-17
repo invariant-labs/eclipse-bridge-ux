0383f5686762aea8fd390bd3e32288086a97f442
Jakub Kowolik <jakubkowolik00@gmail.com>
2025-12-17 20:44:34 +0100

    add transaction history

--- app/(mint-teth)/components/MintAndRedeem.tsx
+++ app/(mint-teth)/components/MintAndRedeem.tsx
@@ -1,11 +1,11 @@
 import classNames from "classnames";
 import { useState } from "react";
-import { Mint } from "./Mint";
 import { Redeem } from "./Redeem";
 import "./styles.css";
 import { NucleusActivityContent } from "./NucleusActivityContent";
 import { Activity } from "@/app/components/icons";
 import { useTransactions } from "../hooks/useTransactions";
+import { Mint } from "./Mint";
 
 export enum Tabs {
   Mint,
@@ -22,6 +22,44 @@ function MintAndRedeem() {
       <div>
         <div className="deposit-container flex flex-col">
           <div className="deposit-card">
+            <div className="header-tabs">
+              <div
+                className={classNames(
+                  "header-tab pointer-events-none opacity-50 cursor-not-allowed",
+                  activeTab === Tabs.Mint ? "active" : "inactive"
+                )}
+                style={{ width: "100%" }}
+                onClick={() => setActiveTab(Tabs.Mint)}
+              >
+                Mint
+              </div>
+              <div
+                className={classNames(
+                  "header-tab",
+                  activeTab === Tabs.Redeem ? "active" : "inactive"
+                )}
+                style={{ width: "100%" }}
+                onClick={() => setActiveTab(Tabs.Redeem)}
+              >
+                Redeem
+              </div>
+              <div
+                className={classNames(
+                  "header-tab",
+                  "flex",
+                  "items-center",
+                  "justify-center",
+                  activeTab === Tabs.Activity ? "active" : "inactive"
+                )}
+                style={{ width: "131px" }}
+                onClick={() => {
+                  setActiveTab(Tabs.Activity);
+                }}
+              >
+                <Activity activityClassName="" />
+              </div>
+            </div>
+            {activeTab === Tabs.Mint && <Mint />}
             {activeTab === Tabs.Redeem && <Redeem />}
             {activeTab === Tabs.Activity && (
               <NucleusActivityContent
